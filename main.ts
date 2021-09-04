a2ba952eb4cec70b0662ab147b0f5e9391149734
Markus Stäuble <staeuble@gmail.com>
2021-09-04 13:50:24 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    einsWeiter()
+    led.plot(x, y)
 })
+function einsWeiter () {
+    led.unplot(x, y)
+    if (x == 4) {
+        y = (y + 1) % 5
+        x = 0
+    } else {
+        x = (x + 1) % 5
+    }
+}
+let y = 0
+let x = 0
+x = 0
+y = 0
+led.plot(x, y)
